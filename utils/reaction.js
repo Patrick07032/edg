d5304310f51d73b6b2b06351765e245a6e53bbfb
Patrick07032 <89949634+Patrick07032@users.noreply.github.com>
2021-09-02 02:26:29 +0100

    Add Event
    Update reaction.js of the site

--- utils/reaction.js
+++ utils/reaction.js
@@ -1,1 +1,22 @@
-//alert("JavaScript is working also...");
+// alert("JS is working also...");
+"use strict";
+
+const login_form = document.getElementById("form");
+const fp_form = document.getElementById("forgotpasswordform");
+const fp_btn = document.getElementById("fp_btn");
+const ca_btn = document.getElementById("ca_btn");
+const el_btn = document.getElementById("el_btn");
+
+fp_btn.addEventListener("click", () => {
+    fp_form.style.display = "block";
+    login_form.style.display = "none";
+    fp_btn.style.display = "none";
+    ca_btn.style.display = "none";
+});
+
+el_btn.addEventListener("click", () => {
+    fp_form.style.display = "none";
+    login_form.style.display = "block";
+    fp_btn.style.display = "block";
+    ca_btn.style.display = "block";
+});
